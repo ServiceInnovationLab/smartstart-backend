01adeb7958a2e80ca9624da689142e8d68ed8aaa
Joe Guo <joeg@catalyst.net.nz>
2016-08-01 09:23:25 +1200

    RM35400: add api to return current user

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,23 +1,31 @@
 from django.conf import settings
 from django.contrib.auth.models import User
-from rest_framework import serializers, viewsets
 from django.contrib.auth.views import login as auth_login
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.response import Response
+from rest_framework.decorators import list_route
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.serializers import HyperlinkedModelSerializer
 from apps.sp.views import login as sp_login
 import logging
 log = logging.getLogger(__name__)
 
 
 # Serializers define the API representation.
-class UserSerializer(serializers.HyperlinkedModelSerializer):
+class UserSerializer(HyperlinkedModelSerializer):
     class Meta:
         model = User
         fields = ('url', 'username', 'email', 'is_staff')
 
 
-# ViewSets define the view behavior.
-class UserViewSet(viewsets.ModelViewSet):
+class UserViewSet(ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+    @list_route(methods=['get'], permission_classes=[IsAuthenticated])
+    def me(self, request):
+        serializer = UserSerializer(request.user, context={'request': request})
+        return Response(serializer.data)
 
 
 def login_router(request):

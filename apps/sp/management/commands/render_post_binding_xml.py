f7cd5c79cb0f224c7c2d14a2ea7d5c0bb15044a8
Joe Guo <joeg@catalyst.net.nz>
2016-08-03 12:41:13 +1200

    add more command line options to render_post_binding_xml command

--- apps/sp/management/commands/render_post_binding_xml.py
+++ apps/sp/management/commands/render_post_binding_xml.py
@@ -1,4 +1,4 @@
-from django.core.management.base import BaseCommand
+from django.core.management.base import BaseCommand, CommandError
 from django.template.loader import render_to_string
 from django.conf import settings
 from ...idps import get_idp
@@ -7,16 +7,33 @@ from ...idps import get_idp
 class Command(BaseCommand):
     help = "Render POST Binding xml"
 
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '--noinput', '--no-input',
+            action='store_false', dest='interactive', default=True,
+            help="Do NOT prompt the user for input of any kind.",
+        )
+
     def handle(self, *args, **options):
-        template = 'sp/SP_PostBinding.xml'
+        site_url = getattr(settings, 'SITE_URL')
+        if not site_url:
+            raise CommandError('Please specify SITE_URL in settings')
+
+        if options['interactive']:
+            msg = "\nYou're going to use this SITE_URL: \n\n    {}\n\nIs it correct?\n\nType 'yes' to continue, or 'no' to cancel: ".format(site_url)
+            if input(msg).lower() != 'yes':
+                raise CommandError("Render cancelled.")
+
         idp = get_idp(settings.IDP)
         if not idp:
-            self.stderr.write('Invalid IDP in settings: {}'.format(settings.IDP))
-            return
+            raise CommandError('Invalid IDP in settings: {}'.format(settings.IDP))
+
+        template = 'sp/SP_PostBinding.xml'
         ctx = {
             'entity_id': idp.entity_id,
             'full_acs_url': idp.acs_url,
             'sp_cer': idp.sp_cer,
         }
         text = render_to_string(template, ctx)
+        self.stdout.write('\n{0}PostBinding XML{0}\n\n'.format('=' * 20))
         self.stdout.write(text)

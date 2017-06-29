1fcd6a8acaf45b142281570f11c4ed59962b7e4b
Joe Guo <joeg@catalyst.net.nz>
2017-06-29 15:04:17 +1200

    add name option to generate_users cmd and mv it to accounts

--- apps/accounts/management/commands/generate_users.py
+++ apps/accounts/management/commands/generate_users.py
@@ -7,13 +7,20 @@ from apps.timeline import models as m
 class Command(BaseCommand):
     help = 'Generate fake users in all phases'
 
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '--name',
+            help='given "joeg", will generate email joeg+weekN@catalyst.net.nz'
+        )
+
     def handle(self, *args, **options):
+        name = options.get('name') or 'lef-dev'
         today = date.today()
         for weekno in range(-2, 73):
             due_date = today + timedelta(weeks=m.PREGNANCY_TOTAL_WEEKS-weekno)
             username = 'week{}'.format(weekno)
-            email = 'lef-dev+{}@catalyst.net.nz'.format(username)
-            user, created = User.objects.update_or_create(username=username, email=email)
+            email = '{}+{}@catalyst.net.nz'.format(name, username)
+            user, created = User.objects.update_or_create(username=username, defaults={'email': email})
             profile = user.profile
             profile.set_due_date(due_date)
-            self.stdout.write('{} user {}'.format('create' if created else 'update', username))
+            self.stdout.write('{} user {}'.format('create' if created else 'update', email))

4cc5383e3966dfcdf3d5c68c31b8a2525b75e838
Joe Guo <joeg@catalyst.net.nz>
2017-06-30 12:01:11 +1200

    mv bootstrap.sh to bin and call in ci-unit-tests.sh

--- bin/ci-unit-tests.sh
+++ bin/ci-unit-tests.sh
@@ -3,6 +3,8 @@
 ROOTDIR=$(dirname $(dirname $0))
 export PYTHONPATH=$ROOTDIR:$PYTHONPATH
 
+$ROOTDIR/bin/bootstrap.sh
+
 virtualenv -p python3 venv
 . $ROOTDIR/venv/bin/activate
 $ROOTDIR/venv/bin/pip install -U -r requirements.txt

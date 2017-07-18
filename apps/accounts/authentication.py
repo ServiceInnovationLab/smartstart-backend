4068313a6931a9ee543f7a2149c46025af68e0df
Grant Patterson <grant.patterson@catalyst.net.nz>
2017-07-19 11:03:14 +1200

    Enforce CSRF validation on session API
    CSRF cookie is always set by phase-metadata response

--- apps/accounts/authentication.py
+++ apps/accounts/authentication.py
@@ -23,3 +23,21 @@ class FixedSessionAuthentication(authentication.SessionAuthentication):
                     logout(request._request)
                     raise FixedSessionTimeout()
         return user_auth
+
+
+class AnonymousSessionAuthentication(authentication.SessionAuthentication):
+    """
+    Enforces CSRF validation on anonymous sessions. This is done in an
+    Authentication class for consistency with SessionAuthentication, where
+    Django Rest Framework normally validates the CSRF token
+    """
+
+    def authenticate(self, request):
+
+        # Get the session-based user from the underlying HttpRequest object.
+        # If user hasn't authenticated, Django provides an AnonymousUser object
+        user = getattr(request._request, 'user', None)
+
+        self.enforce_csrf(request)
+
+        return user, None
--- apps/base/permissions.py
+++ apps/base/permissions.py
@@ -1,5 +1,6 @@
 from rest_framework import permissions
 
+
 class ReadOnly(permissions.BasePermission):
     """
     ReadOnly to all

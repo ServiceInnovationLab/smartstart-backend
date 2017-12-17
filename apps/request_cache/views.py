888ab1c65eff672cfb043dfeeb315bc993472e08
Jonathan Harker <jonathan@catalyst.net.nz>
2017-12-18 12:24:31 +1300

    LBS permissions to allow any, fix timezone and OK check.

--- apps/request_cache/views.py
+++ apps/request_cache/views.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
+from rest_framework.permissions import AllowAny
 from apps.request_cache.models import RequestCache
 
 
@@ -13,41 +14,49 @@ def ckan_result_response(name):
 
 
 @api_view()
+@permission_classes((AllowAny,))
 def lbs_parenting_support(request):
     return ckan_result_response('lbs_parenting_support')
 
 
 @api_view()
+@permission_classes((AllowAny,))
 def lbs_early_education(request):
     return ckan_result_response('lbs_early_education')
 
 
 @api_view()
+@permission_classes((AllowAny,))
 def lbs_breastfeeding(request):
     return ckan_result_response('lbs_breastfeeding')
 
 
 @api_view()
+@permission_classes((AllowAny,))
 def lbs_antenatal(request):
     return ckan_result_response('lbs_antenatal')
 
 
 @api_view()
+@permission_classes((AllowAny,))
 def lbs_mental_health(request):
     return ckan_result_response('lbs_mental_health')
 
 
 @api_view()
+@permission_classes((AllowAny,))
 def lbs_budgeting(request):
     return ckan_result_response('lbs_budgeting')
 
 
 @api_view()
+@permission_classes((AllowAny,))
 def lbs_well_child(request):
     return ckan_result_response('lbs_well_child')
 
 
 @api_view()
+@permission_classes((AllowAny,))
 def content_timeline(request):
     # DIA are whitelisting this API by user agent.
     result = RequestCache.objects.filter(name='content_timeline').first()
